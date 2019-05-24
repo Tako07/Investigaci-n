43a3a3a87d82c03f6749d6bad339d4e3975860e1
Takeshi <g.t.n.ramirez@gmail.com>
2019-05-24 01:41:33 -0500

    Micro correción

--- investigacion/index.php
+++ investigacion/index.php
@@ -69,7 +69,7 @@
             </div>
             <div class="col m9">
               <p><strong>Hamburgruesa</strong><br>
-    					Está bien sabrosonga <br>
+    					La favorita del público <br>
     					<strong>Descripción</strong><br>
     					Lleva jitomate <br>
     					y cebolla<br>
